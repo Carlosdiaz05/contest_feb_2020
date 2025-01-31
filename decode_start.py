a202f5105379800216c1f664f99e28f822780f54
Carlos <carlosdj0305@gmail.com>
2025-01-31 13:22:56 +0100

    contest solution

--- decode_start.py
+++ decode_start.py
@@ -3,4 +3,10 @@ vowel = "aeiou"
 message = ""
 with open("secret.txt") as f:
     for line in f:
-        print(line) # instead of print, you should start decoding
+        num_vowels = 0
+        for letter in line:
+            if letter in vowel: # letter is a vowel
+                num_vowels += 1
+        #decode here
+        message += alphabet[num_vowels]
+print(message)
